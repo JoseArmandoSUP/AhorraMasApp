abd2ba601f969f3fe45297e22ed0a7d65e3fcccd
JoseArmandoSUP <124048843@upq.edu.mx>
2025-10-27 10:16:44 -0600

    Mandar a llamar la pantalla principal

--- ahorraMasApp/App.js
+++ ahorraMasApp/App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import PantallaPrincipal from './screens/PantallaPrincipal';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PantallaPrincipal></PantallaPrincipal>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
