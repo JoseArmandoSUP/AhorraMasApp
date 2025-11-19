39b47547caa755e891af9b8c80a86090b4d37052
JoseArmandoSUP <124048843@upq.edu.mx>
2025-11-18 20:20:52 -0600

    NavigationStack completada

--- ahorraMasApp/App.js
+++ ahorraMasApp/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+import { inicializarBasedatos } from './database/database';
 import PantallaPrincipal from './screens/PantallaPrincipal';
 
 export default function App() {
+  
+  useEffect(()=>{
+    inicializarBasedatos()
+      .then(()=>console.log("Base de Datos cargada"))
+      .catch((err)=>console.log("Error al cargar BD:", err));
+  }, []);
+  
   return (
     <PantallaPrincipal></PantallaPrincipal>
   );
