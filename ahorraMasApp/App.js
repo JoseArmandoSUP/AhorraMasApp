193057572fc4722f48e63608838096aa08e1697a
JoseArmandoSUP <124048843@upq.edu.mx>
2025-11-20 10:34:39 -0600

    Borrar lo de SQLite porque se rompió

--- ahorraMasApp/App.js
+++ ahorraMasApp/App.js
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
-import { inicializarBasedatos } from './database/database';
 import PantallaPrincipal from './screens/PantallaPrincipal';
 
 export default function App() {
-  
-  useEffect(()=>{
-    inicializarBasedatos()
-      .then(()=>console.log("Base de Datos cargada"))
-      .catch((err)=>console.log("Error al cargar BD:", err));
-  }, []);
   
   return (
     <PantallaPrincipal></PantallaPrincipal>
