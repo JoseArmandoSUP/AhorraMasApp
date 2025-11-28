f3f11d83e692c9fd7242c75fa5b8b315891889e5
JoseArmandoSUP <124048843@upq.edu.mx>
2025-11-27 23:54:43 -0600

    Plantilla base para la pantalla de perfil

---
+++ ahorraMasApp/screens/Perfil.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Perfil() {
+  return (
+    <View>
+      <Text>Pantalla de perfil</Text>
+    </View>
+  );
+}
